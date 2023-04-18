acdbed49de9fdd0178654422d15a5c721dad1875
최왕규 <wanggyu@mrpsoft.co.kr>
2023-04-18 10:23:15 +0900

    init setting

---
+++ src/main/java/com/example/batchtest/domain/Member.java
@@ -0,0 +1,29 @@
+package com.example.batchtest.domain;
+
+import com.example.batchtest.domain.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "amount")
+    private BigDecimal amount;
+
+
+
+
+}
